08b7c32b13841c331e0d3f028dc425923827cb19
Andrey Antonov <andrey10183@gmail.com>
2021-09-21 00:23:50 +0300

    MyLib added

---
+++ MyLib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class Class1
+    {
+        //class 1
+        //class 2
+        //class 3
+    }
+}
---
+++ MyLib/MyClass/Calc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib.MyClass
+{
+    class Calc
+    {
+        //1;
+        //2;
+        //3;
+    }
+}
