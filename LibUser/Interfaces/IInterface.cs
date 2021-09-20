974f572e70eb805de6dd2eeb0ea08cd5ce53a93b
Andrey Antonov <andrey10183@gmail.com>
2021-09-21 00:22:24 +0300

    LibUser added

---
+++ LibUser/Interfaces/IInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibUser.Interfaces
+{
+    interface IInterface
+    {
+        //int 1
+        //int 2
+        //int 3
+    }
+}
---
+++ LibUser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
